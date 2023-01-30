7416c9bf4c4325a8b507daf3f4ddfde8612da2ab
KallappaM <115877818+KallappaM@users.noreply.github.com>
2023-01-30 19:03:25 +0530

    Update Implementation.c

--- Implementation.c
+++ Implementation.c
@@ -6,8 +6,8 @@ void CreateInformat(int lowLimit,int highLimit,int counter, char* buff)
 {
   
 char Data[100];
-sprintf(Data,"%s\n%d-%d, %d\n",CSVFormat,lowLimit,highLimit,counter);
-strncpy(buff,Data,strlen(Data));
+sprintf(buff,"%s\n%d-%d, %d\n",CSVFormat,lowLimit,highLimit,counter);
+//strncpy(buff,Data,strlen(Data));
 }
 void doSort(int *A,int len)
 {
