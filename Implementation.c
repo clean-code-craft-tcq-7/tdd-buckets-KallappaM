7f7f55eba1cfc6b7eff3f3041f6280ba03d0cead
KallappaM <115877818+KallappaM@users.noreply.github.com>
2023-01-30 19:25:55 +0530

    Update Implementation.c

--- Implementation.c
+++ Implementation.c
@@ -5,7 +5,7 @@
 void CreateInformat(int lowLimit,int highLimit,int counter, char* buff)
 {
   
-char Data[100];
+//char Data[100];
 sprintf(buff,"%s\n%d-%d, %d\n",CSVFormat,lowLimit,highLimit,counter);
 //strncpy(buff,Data,strlen(Data));
 }
