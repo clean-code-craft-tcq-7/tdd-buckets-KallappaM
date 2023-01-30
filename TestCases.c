8d9a74c8ab7bb1648af9980c2e1fa52ce6a39cf9
KallappaM <115877818+KallappaM@users.noreply.github.com>
2023-01-30 19:41:25 +0530

    Update TestCases.c

--- TestCases.c
+++ TestCases.c
@@ -7,11 +7,17 @@
 int main(void)
 {
  
+ int current_samples[7]={3, 3, 5, 4, 10, 11, 12};
+ int storecount;
  char output[100];
  memset(output, 0, 100);
  
-CreateInformat(4,5,2, output);
+
+doSort(current_samples,7);
+storecount = CountValue(current_samples,7,3,5);
+CreateInformat(3,5,storecount,output);
  
+ printf("%s", output);
  assert(strcmp(output,"Range, Readings\n4-5, 2\n") == 0);
   
 return 0;
