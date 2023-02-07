ca43a5b4d0038ec358e92ae6a7aaa0e0cee0001e
KallappaM <115877818+KallappaM@users.noreply.github.com>
2023-02-07 21:05:43 +0530

    Update TestCases.c

--- TestCases.c
+++ TestCases.c
@@ -51,7 +51,7 @@ printf("%s", output);
 assert(strcmp(output,"Range, Readings\n4-5, 0\n") == 0);*/
  
  int Sample1[] = {0,1,1024,4096,2048,4094,6000};
- GloableLen = sizeof(Sample1)/sizeof(Sample1[0]);
+ GloableLen = (int const)(sizeof(Sample1)/sizeof(Sample1[0]));
  int *p,leng;
  p = RemoveUnwanted(Sample1,12, GloableLen);
  leng = LEN(Sample1);
